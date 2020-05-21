509fb1c08ee3282e45c94325df130aa7e42c3797
amanpreet9391 <amanpreet9391@gmail.com>
2020-05-21 13:04:20 +0530

    create security group

--- prod.tf
+++ prod.tf
@@ -9,3 +9,29 @@ resource "aws_s3_bucket" "my-s3-prod-bucket"{
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod-security-group"{
+
+  
+  ingress{
+    from_port     =  40
+    to_port       =  40
+    protocol      =  "tcp"
+    cidr_blocks   =  ["0.0.0.0/0"]
+     
+  }
+
+  ingress{
+    from_port    =  443
+    to_port      =  443
+    protocol     =  "tcp"
+    cidr_blocks  =  ["0.0.0.0/0"]
+  }
+
+  egress{
+    from_port    =  0
+    to_port      =  0
+    protocol     =  "-1"
+    cidr_blocks  =  ["0.0.0.0/0"]
+  }  
+}
